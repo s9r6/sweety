cd3def9b7309cb5e16a7b5993dd3838cfc9c6cff
s9r6 <saranyaa96thiyagu@gmail.com>
2017-06-05 22:36:32 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class oddeve
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        {
+            if(a!=0)
+        {
+        if(a%2==0)
+{
+    System.out.println(" even");
+} 
+else if(a%2!=0)
+    {
+        System.out.println(" odd");
+    }
+        }
+     else
+    {
+        System.out.println("  zero");
+    }
+}
+        
+    }
+}
+    
